14eb587757437d1e1a48f7a86a2d5700caaf1a07
ankushransiwal <ankushransiwal.13@gmail.com>
2019-09-05 13:23:48 +0530

    Create Deck class and import random to shuffle the deck

--- Blackjack.py
+++ Blackjack.py
@@ -15,3 +15,24 @@ class Card:
     
     def __str__(self):
         return self.rank +' of ' +self.suit
+
+class Deck:
+    
+    def __init__(self):
+        self.deck = []  # start with an empty list
+        for suit in suits:
+            for rank in ranks:
+                self.deck.append(Card(suit,rank)) # Build Card objects and add them to the list
+    
+    def __str__(self):
+        deck_value = ''
+        for card in self.deck:
+            deck_value += '\n '+card.__str__()
+        return ' The deck has :\n' + deck_value
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+        
+    def deal(self):
+        single_card = self.deck.pop()
+        return single_card
