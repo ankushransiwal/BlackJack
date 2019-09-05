0d0aeec5f038499e0b0666ddfc3c55223f68418c
ankushransiwal <ankushransiwal.13@gmail.com>
2019-09-05 13:25:26 +0530

    Create chips class - to handle player chips

--- Blackjack.py
+++ Blackjack.py
@@ -57,3 +57,15 @@ class Hand:
     
     def adjust_for_ace(self):
         pass
+
+class Chips:
+    
+    def __init__(self):
+        self.total = 200  # This can be set to a default value or supplied by a user input
+        self.bet = 0
+        
+    def win_bet(self):
+        self.total += self.bet
+    
+    def lose_bet(self):
+        self.total -= self.bet
